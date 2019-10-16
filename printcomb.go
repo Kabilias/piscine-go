5860492c77d40d97720b0268f48e99fc1edd77c7
Kabilias <wswcf@mail.ru>
2019-10-16 19:07:16 +0600

    printcomb.go

---
+++ printcomb.go
@@ -0,0 +1,24 @@
+package PrintComb
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i < j {
+					if j < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						if i < '7' {
+							z01.PrintRune(',')
+							z01.PrintRune(32)
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune(10)
+}

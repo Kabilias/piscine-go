795a3cb924d694c2c78419b5929d0efd824e39ed
Kabilias <wswcf@mail.ru>
2019-10-17 16:13:41 +0600

    x

--- printcomb2.go
+++ printcomb2.go
@@ -3,17 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for q := '0'; q <= '9'; q++ {
-		for w := '0'; w <= '9'; w++ {
-			for e := '0'; e <= '9': e++ {
-				for r := '0'; r <= '9'; r++ {
-					if q == e && w < r '9'; q < e {
-						z01.PrintRune(q)
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9': c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a == c && b < d || a < c {
+						z01.PrintRune(a)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
-						z01.PrintRune(e) 
-						z01.PrintRune(r)
-							if q != '9' || w != '8' || r != '9' || e != '9' {
+						z01.PrintRune(c) 
+						z01.PrintRune(d)
+							if a != '9' || b != '8' || c != '9' || d != '9' {
 								z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}

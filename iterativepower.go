dff87cabc16f08d31f5c248f9b85f21b3c790b46
Kabilias <wswcf@mail.ru>
2019-10-21 16:57:07 +0600

    z

---
+++ iterativepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if IterativePower < 0 {
+		return 0
+	}
+	p := 1
+	for IterativePower > 0 {
+			p = p * nb
+				IterativePower --
+	}
+	return p 
+	}

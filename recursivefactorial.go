9c655b423218be8d194d5b15386e6df67cf3ce7e
Kabilias <wswcf@mail.ru>
2019-10-21 16:31:57 +0600

    a

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	i := 0
+	if nb == 0 {
+		return 1
+	} else if nb < 0 || nb >=13 {
+		return 0
+	} else {
+		i = nb * RF(nb-1)
+	}
+	return a
+}

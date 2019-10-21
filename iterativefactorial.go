1a138e44b692dbcb07e1986ed3b6643ae462bb39
Kabilias <wswcf@mail.ru>
2019-10-21 16:32:43 +0600

    s

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,16 +2,15 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	if nb > 30 {
-		return 0 
+		return 0
 	}
-			if nb < 0 {
-				return 0
-			 }
-			 factorial := 1
-					for nb > 0 {
-						factorial = factorial * nb
-						nb -- 
-					}
-					return factorial
-				}
-	
+	if nb < 0 {
+		return 0
+	}
+	factorial := 1
+	for nb > 0 {
+		factorial = factorial * nb
+		nb--
+	}
+	return factorial
+}

fff866f64d75856feababe73782cd4a23846d2f7
Kabilias <wswcf@mail.ru>
2019-10-21 19:12:24 +0600

    q

--- isprime.go
+++ isprime.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IsPrime(nb int) bool {
-    if get <= 1 {
-        return false
+	if get <= 1 {
+		return false
 	}
 	for a := 2; a < get; a++ {
-		if get % a == 0 {
+		if get%a == 0 {
 			return false
 		}
 	}

787d337570791a7c597626a7a1d8c57291f7b760
Kabilias <wswcf@mail.ru>
2019-10-21 17:28:48 +0600

    ss

--- fibonacci.go
+++ fibonacci.go
@@ -4,11 +4,11 @@ func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
 	}
-	if index == 0 {
-		return 0 
-	}
 	if index == 1 {
 		return 1
+	}
+	if index == 0 {
+		return 0
 	} else {
 		return (Fibonacci(index-2) + Fibonacci(index-1))
 	}

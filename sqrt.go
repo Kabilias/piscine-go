47ede0aefabf4dfb817d7dab9bdf31e6971d7ccd
Kabilias <wswcf@mail.ru>
2019-10-21 18:47:59 +0600

    a

--- sqrt.go
+++ sqrt.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Sqrt(nb int) int {
-	for a := 0; (a * a) <= nb;
-		if a * a == nb {
+	for a := 0; (a * a) <= nb; r++ {
+		if a*a == nb {
 			return a
 		}
-}
-return 0
+	}
+	return 0
 }

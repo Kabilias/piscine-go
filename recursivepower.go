7f095e01f9ef33b70aaf51483eddcc2c97ada4b0
Kabilias <wswcf@mail.ru>
2019-10-21 17:13:38 +0600

    cc

--- recursivepower.go
+++ recursivepower.go
@@ -4,10 +4,9 @@ func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	} else if power == 1 {
-        return nb 
-    }
-	else if power < 0 {
-			return 0
-}
-return nb * RecursivePower(nb, power-1)
+		return nb
+	} else if power < 0 {
+		return 0
+	}
+	return nb * RecursivePower(nb, power-1)
 }

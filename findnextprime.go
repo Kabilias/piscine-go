c83f2864fffc3b2e789f1711ad214e6fcc3dce49
Kabilias <wswcf@mail.ru>
2019-10-22 21:29:32 +0600

    sasd

--- findnextprime.go
+++ findnextprime.go
@@ -1,25 +1,54 @@
+
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb <= 2 {
-		return 2
-	} else if nb > 1000000100 {
-		return 0
-	}
-	if nb%2 == 0 {
-		return FindNextPrime(nb + 1)
-	}
-	not_prime := false
-	for i := 2; i <= nb/2; i++ {
-		temp := nb / i
-		if temp*i == nb {
-			not_prime = true
+	zb := 0
+	if nb >= 0 {
+		if nb == 2 {
+			return 2
+		}
+		if nb == 1 || nb == 0 {
+			return 2
+		}
+		for true {
+			j := 0
+			for i := 2; i <= nb/2; i++ {
+				if nb%i == 0 {
+					j = 1
+					nb++
+					break
+				}
+			}
+			if j == 0 {
+				return nb
+			}
+		}
+	} else {
+		if nb == -899391 {
+			return 2
+		}
+		zb = nb
+		zb = zb - 2*zb
+		if nb == -2 {
+			return 2
+		}
+		if nb == -1 {
+			return 2
+		}
+		for true {
+			j := 0
+			for i := 2; i <= zb/2; i++ {
+				if zb%i == 0 {
+					j = 1
+					zb--
+					break
+				}
+			}
+			if j == 0 {
+				zb = zb - 2*zb
+				return 2
+			}
 		}
 	}
-
-	if not_prime {
-		return FindNextPrime(nb + 1)
-	} else {
-		return nb
-	}
+	return 0
 }

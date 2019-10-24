406bae3aa00e2e830903cef86c7e0df55e76e9dc
Kabilias <wswcf@mail.ru>
2019-10-24 18:01:33 +0600

    a

--- firstrune.go
+++ firstrune.go
@@ -1,13 +1,6 @@
 package piscine
 
 func FirstRune(s string) rune {
-	runes := []rune(s)
-	length := 0
-	for i := range s {
-		length = i + 1
-	}
-	if length == 0 {
-		return -1
-	}
-	return runes[0]
+	arrayStr := []rune(s)
+	return arrayStr[0]
 }

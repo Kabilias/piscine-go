7ba161da781c4d0524e2785ee535212c49fe3eff
Kabilias <wswcf@mail.ru>
2019-10-24 19:09:05 +0600

    asd

--- toupper.go
+++ toupper.go
@@ -1,17 +1,13 @@
 package piscine
 
 func ToUpper(s string) string {
-	h := []rune(s)
-	result := ""
-	for i := 0; i <= lent(h)-1; i++ {
+	runes := []rune(s)
 
-		if (h[i] >= 'a') && (h[i] <= 'z') {
-
-			h[i] = h[i] - 32
-
+	for i := range s {
+		if runes[i] > 96 && runes[i] < 123 {
+			runes[i] = runes[i] - 32
 		}
-		result += string(h[i])
-
 	}
-	return result
+	ss := string(runes)
+	return ss
 }
